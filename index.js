9af086ce7042471685ba03f3581c25fb23b6068a
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-11-23 18:51:49 +0000

    spread operator

--- index.js
+++ index.js
@@ -8,12 +8,16 @@
 
 // button.addEventListener('click', fn2); 
 
-function sumUp(...toAdd) {
-    let result = 0;
-    for (let i = 0; i < toAdd.length; i++) {
-        result += toAdd[i];
-    }
-    return result;
-}
+// function sumUp(...toAdd) {
+//     let result = 0;
+//     for (let i = 0; i < toAdd.length; i++) {
+//         result += toAdd[i];
+//     }
+//     return result;
+// }
 
-console.log(sumUp(100, 10, 20));
+// console.log(sumUp(100, 10, 20));
+
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(Math.max(...numbers));
