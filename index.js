46fa572e0bf273afe369d6916b1f16ce397afcb3
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-11-23 15:36:18 +0000

    this keywod

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const AGES = [27, 29, 31];
-console.log(AGES);
+// const AGES = [27, 29, 31];
+// console.log(AGES);
+
+var button = document.querySelector('button');
+
+var fn2 = () => console.log(this);
+
+
+button.addEventListener('click', fn2);
