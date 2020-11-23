1365d7c142a1698b6e646c9bac4b9bc35613060b
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-11-23 18:47:16 +0000

    rest operator

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// const AGES = [27, 29, 31];
-// console.log(AGES);
+// // const AGES = [27, 29, 31];
+// // console.log(AGES);
 
-var button = document.querySelector('button');
+// var button = document.querySelector('button');
 
-var fn2 = () => console.log(this);
+// var fn2 = () => console.log(this);
 
 
-button.addEventListener('click', fn2);
+// button.addEventListener('click', fn2); 
+
+function sumUp(...toAdd) {
+    let result = 0;
+    for (let i = 0; i < toAdd.length; i++) {
+        result += toAdd[i];
+    }
+    return result;
+}
+
+console.log(sumUp(100, 10, 20));
