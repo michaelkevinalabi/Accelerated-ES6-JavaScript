04b0ff4a3e9fddb71f99f1188bdf5ac8d10ba67f
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-11-27 09:40:35 +0000

    Weather data class with reflect adn proxies

--- Course Project/app.js
+++ Course Project/app.js
@@ -1,11 +1,20 @@
-import * as ELEMENTS from 'elements.js';
+import * as ELEMENTS from "elements.js";
+import { Http } from "http.js";
 
-ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
+const APP_ID = '';
+
+ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener("click", searchWeather);
 
 function searchWeather() {
-    const CITY_NAME = ELEMENTS.ELEMENT_SEARCH_CITY.value.trim();
-    if (CITY_NAME.length == 0) {
-        return alert('Please enter a city name');
-    }
-    alert(CITY_NAME);
+  const CITY_NAME = ELEMENTS.ELEMENT_SEARCH_CITY.value.trim();
+  if (CITY_NAME.length == 0) {
+    return alert("Please enter a city name");
+  }
+  const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APP_ID;
+
+  Http.fetchData(URL)
+    .then(responseData => {
+        
+    })
+  .catch(error => alert(error));
 }
